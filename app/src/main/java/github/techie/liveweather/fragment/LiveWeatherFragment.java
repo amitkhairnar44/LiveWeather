a48c41fd709a24a040bfb13f09982aefb45822f6
Grace5921 <androidlover5842@gmail.com>
2016-11-17 16:50:29 +0530

    add live weather fragment and ads activity

---
+++ app/src/main/java/github/techie/liveweather/fragment/LiveWeatherFragment.java
@@ -0,0 +1,24 @@
+package github.techie.liveweather.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import github.techie.liveweather.R;
+
+/**
+ * Created by sumit on 17/11/16.
+ */
+
+public class LiveWeatherFragment extends Fragment {
+    @Nullable
+    @Override
+
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        final View view = inflater.inflate(R.layout.fragment_live_weather, container, false);
+        return view;
+    }
+}
